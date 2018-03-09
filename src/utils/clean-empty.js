c500272688dd69c4a50c112266c38f49d4e90ac0
Hans Yulian <hansyulian@windowslive.com>
2018-03-09 17:57:49 +0800

    forgot to save

--- src/utils/clean-empty.js
+++ src/utils/clean-empty.js
@@ -1,4 +1,4 @@
-module.exports = function cleanUndefined(obj) {
+module.exports = function cleanEmpty(obj) {
     for (var key in obj)
         if (obj[key] === null || obj[key] === undefined)
             delete obj[key]
